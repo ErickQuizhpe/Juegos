477fe02e53c99431a8487fd6d5b0c7223db53251
Paul Andres <paulandres.cuenta@gmail.com>
2025-01-11 18:04:09 -0500

    'Agregando archivo de configuracion al proyecto'

--- src/App.js
+++ src/App.js
@@ -8,13 +8,35 @@ import IdentificacionUnidadesJuego from './componets/IdentificacionUnidadesJuego
 function App() {
   return (
     <Router>
-        <Routes>
+      <Routes>
+        {/* Página principal */}
         <Route path="/" element={<HomePage />} />
+        
+        {/* Página de juegos */}
         <Route path="/games" element={<GamesPage />} />
+        
+        {/* Página para juegos específicos */}
         <Route path="/game/:operation" element={<IdentificacionUnidadesJuego />} />
+        
+        {/* Ruta para la página HTML de multiplicaciones-unidades */}
+        <Route
+          path="/multiplicaciones-unidades"
+          element={
+            <iframe
+              src="./pages/MultiplicacionesUnidades.html"
+              style={{
+                width: '100%',
+                height: '100vh',
+                border: 'none',
+              }}
+              title="Multiplicaciones Unidades"
+            />
+          }
+        />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
--- src/pages/Gamepage.js
+++ src/pages/Gamepage.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom'; 
-import '../styles/global.css';  
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/global.css";
 
 const GamesPage = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const goToGame = (operation) => {
-    navigate(`/game/${operation}`); 
+    navigate(`/game/${operation}`);
   };
   const handleLogout = () => {
     // Redirigir a la página principal (o cualquier otra página de salida)
-    navigate('/'); 
+    navigate("/");
   };
   return (
     <div className="games-page">
       <h1>Selecciona un juego</h1>
       <div className="button-container">
-        <button onClick={() => goToGame('addition')}>Suma</button>
-        <button onClick={() => goToGame('subtraction')}>Resta</button>
-        <button onClick={() => goToGame('multiplication')}>Multiplicación</button>
-        <button onClick={() => goToGame('division')}>División</button>
+        <button onClick={() => goToGame("addition")}>Suma</button>
+        <button onClick={() => goToGame("subtraction")}>Resta</button>
+        <button onClick={() => goToGame("multiplication")}>Multiplicación</button>
+        <button onClick={() => goToGame("division")}>División</button>
+        <button onClick={() => goToGame("multiplicaciones-unidades")}>
+          TEST
+        </button>
       </div>
-      <button className="logout-button" onClick={handleLogout}>Salir</button>
+      <button className="logout-button" onClick={handleLogout}>
+        Salir
+      </button>
     </div>
   );
 };
 
 export default GamesPage;
-
