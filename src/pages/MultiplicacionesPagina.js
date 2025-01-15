f7a1d66cdad67d50f9229c46dc065fc3c01a8c2f
Paul Andres <paulandres.cuenta@gmail.com>
2025-01-15 09:40:05 -0500

    "Actualizacion del seccion de navegaicion entre paginas dentro del apartado de multiplicacion"

--- src/pages/MultiplicacionesPagina.js
+++ src/pages/MultiplicacionesPagina.js
@@ -8,66 +8,46 @@ function Multiplicaciones() {
     <div>
       <div className="bg-gray-100 min-h-screen">
         {/* Header */}
-        <header
-          className=" header bg-blue-
-        text-balance text-white py-3 text-center shadow-md"
-        >
+        <header className="header bg-blue-600 text-white py-3 text-center shadow-md">
           <h1 className="text-6xl font-bold">Multiplicación de Números</h1>
         </header>
 
         <div className="grid">
           {/* Navigation */}
-          <nav className="nav bg-orange-300">
-            <h2 className="text-xl font-semibold mb-4">Elige otra operación</h2>
-            <ul className="space-y-2">
+          <nav className="nav">
+            <h2>Elige otra operación</h2>
+            <ul>
               <li>
-                <a href="#" className="text-blue-600 hover:underline">
-                  Sumas
-                </a>
+                <a href="#">Sumas</a>
               </li>
               <li>
-                <a href="#" className="text-blue-600 hover:underline">
-                  Restas
-                </a>
+                <a href="#">Restas</a>
               </li>
               <li>
-                <a href="#" className="text-blue-600 hover:underline">
-                  Multiplicación
-                </a>
+                <a href="#">Multiplicación</a>
               </li>
               <li>
-                <a href="#" className="text-blue-600 hover:underline">
-                  División
-                </a>
+                <a href="#">División</a>
               </li>
             </ul>
           </nav>
 
           {/* Main Content */}
           <main className="main">
-            <h2 className="text-3xl font-bold mb-4">
+            <h2 className="text-2xl font-bold mb-4">
               Resuelve el ejercicio a continuación
             </h2>
             <div className="cuadricula">{<Prueba />}</div>
           </main>
 
           {/* Aside Section */}
-          <aside
-            className="aside bg-pink-200
-      rounded-md shadow-lg p-6  hover:shadow-xl transition-shadow duration-300"
-          >
-            <h2
-              className="text-3xl font-semibold mb-3
-          txt-center"
-            >
-              Tablas de multiplicar
-            </h2>
+          <aside className="aside bg-pink-200 rounded-md shadow-lg p-6">
+            <h2 className="text-3xl font-semibold mb-3">Tablas de multiplicar</h2>
             <MultiplicacionDisco />
           </aside>
 
-          Footer
           <footer className="footer bg-gray-800 text-white py-4 text-center">
-            <p>UCAUCUE © Copyright 2025</p>
+            <p>UCACUE GRUPO 3 © Copyright 2025</p>
           </footer>
         </div>
       </div>
