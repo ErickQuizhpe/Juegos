f723556317e594ef4efd1e0007ad587cd389edc7
MatiasVz <matiasvel2003@gmail.com>
2025-01-17 00:52:30 -0500

    Actualizar Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,38 +1,39 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "../styles/global.css";
-import unidades from "../assets/UNIDADES.png"
-import operaciones from "../assets/Operaciones.jpg"
+import unidades from "../assets/UNIDADES.png";
+import operaciones from "../assets/Image/OperacionAgruaoacion.jpg";
 
 
 const HomePage = () => {
   const navigate = useNavigate();
-
   return (
     <div style={styles.container}>
-      <h1 style={styles.title}>Seleccione una opción para empezar</h1>
+      <h1 className="font-bold text-5xl text-blue-800" 
+      >SELECCIONE UN ACTIVIDAD</h1>
+
       <div style={styles.optionsContainer}>
-        <div
+        <div 
           style={styles.option}
           onClick={() => navigate("/games?type=operations")}
         >
-          <img
-            src={operaciones}
-            alt="Operaciones"
-            style={styles.icon}
-          />
-          <p>Operaciones con agrupaciones</p>
+          <img className="size-3" src={operaciones}
+           alt="Operaciones" style={styles.icon} />
+          <p className="font-bold text-5xl">Operaciones con agrupaciones</p>
         </div>
+
+
+        
         <div
           style={styles.option}
-          onClick={() => navigate("/games?type=units")}
+          onClick={() => navigate("/OperacionesAgrupaciones")}
         >
           <img
             src={unidades}
             alt="Identificación de unidades"
             style={styles.icon}
           />
-          <p>Identificación unidades y operaciones matemáticas</p>
+          <p>Operaciones Matemáticas</p>
         </div>
       </div>
     </div>
@@ -46,12 +47,16 @@ const styles = {
     alignItems: "center",
     backgroundColor: "#d0f0ff",
     height: "100vh",
+    width: "100%",
     padding: "20px",
   },
   title: {
-    fontSize: "24px",
+    fontSize: "40px",
     color: "#4a4a4a",
     marginBottom: "20px",
+    letterSpacing: "2px",
+    textShadow:
+      "2px 2px 5px rgba(0, 0, 0, 0.7), 0 0 25px rgba(255, 255, 255, 0.6)",
   },
   optionsContainer: {
     display: "flex",
