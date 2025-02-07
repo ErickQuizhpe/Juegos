26bdff4dcd502fe13324d15a2162704b05e565c4
ErickQuizhpe <equizhpe2003@gmail.com>
2025-02-06 22:41:02 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import GamesPage from './pages/GamesPage';
-import React from 'react';
-import OperacionesAgrupacion from './pages/OperacionesAgrupacion';
-import MinusPage from './pages/MinusPage';
-import MultiplicationPage from './pages/MultiplicationPage';
-import DivisionPage from './pages/DivisionPage';
-import AdditionPage from './pages/AdditionPage';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import GamesPage from "./pages/Gamepage";
+import React from "react";
+import IdentificacionUnidadesJuego from "./componets/IdentificacionUnidadesJuego";
+import Multiplicaciones from "./pages/MultiplicationPage";
+import OperacionesAgrupacion from "./pages/OperacionesAgrupacion";
+import MinusPage from "./pages/MinusPage";
+import AdditionPage from "./pages/AdditionPage";
+import DivisionPage from "./pages/DivisionPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* Página principal */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Página de juegos */}
         <Route path="/games" element={<GamesPage />} />
-        <Route path="/AdditionPage" element={<AdditionPage />} />
-        <Route path="/MinusPage" element={<MinusPage />} />
-        <Route path="/Multiplicaciones" element={<MultiplicationPage />} />
-        <Route path="/DivisionPage" element={<DivisionPage />} />
-        <Route path="/OperacionesAgrupacion" element={<OperacionesAgrupacion />} />
+
+        {/* Página para juegos específicos */}
+        <Route
+          path="/game/:operation"
+          element={<IdentificacionUnidadesJuego />}
+        />
+
+        <Route path="/Multiplicaciones" element={<Multiplicaciones />} />
+
+        <Route
+          path="/OperacionesAgrupaciones"
+          element={<OperacionesAgrupacion />}
+        />
+        <Route path="/MinusPage" element={<MinusPage />}></Route>
+        <Route path="/DivisionPage" element={<DivisionPage />}></Route>
+        <Route path="/AdditionPage" element={<AdditionPage />}></Route>
       </Routes>
     </Router>
   );
